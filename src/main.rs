3a31f2d0af24774ded271b6de578ba07bba3a1fd
Faisal Bin Ahmed <faisalbin@outlook.com>
2023-10-19 19:23:52 +0200

    get station info

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![allow(unused)]
+use serde::Deserialize;
+use serde_json::from_str;
 use std::collections::HashMap;
 
 #[tokio::main]
@@ -23,17 +25,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         args: vec![],
     };
 
-    let resp = fetch_url(mvg_zdm, station_ids_endpoint.url).await?;
-    println!("{:#?}", resp);
+    // let resp = fetch_url(mvg_zdm, station_ids_endpoint.url).await?;
+
+    let location_url = format!("{}{}", mvg_fib, LOCATION_URL);
+
+    let resp = fetch_station_info(&location_url, "de:09162:6").await?;
+    // println!("{:#?}", resp);
     Ok(())
 }
 
 async fn fetch_url(base_url: &str, url: &str) -> Result<(), Box<dyn std::error::Error>> {
     let full_url = format!("{}{}", base_url, url);
 
+    let resp = reqwest::get(full_url).await?.json::<Vec<String>>().await?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")] //to avoid renaming all the fields to snake_case
+struct StationInfo {
+    house_number: String,
+    latitude: f32,
+    longitude: f32,
+    name: String,
+    place: String,
+    post_code: String,
+    street: String,
+    // type: String
+}
+
+async fn fetch_station_info(url: &str, query: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let full_url = format!("{}?query={}", url, query);
+
     let resp = reqwest::get(full_url)
         .await?
-        .json::<serde_json::Value>()
+        .json::<Vec<StationInfo>>()
         .await?;
     println!("{:#?}", resp);
     Ok(())
